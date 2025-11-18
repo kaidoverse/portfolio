b9001b7fc145f566dae1e56e1fcd0f8871860755
kaidoverse <kaidoverse@gmail.com>
2025-11-18 13:26:11 +0000

    initial commit with folder structure

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+// import { BrowserRouter, Route, Routes } from 'react-router-dom'
+// import UserLayout from './component/layout/UserLayout'
+// import About from './pages/About'
+// import Contact from './pages/Contact'
+// import Home from './pages/Home'
+// import Services from './pages/Services'
+
+// import { Toaster } from "sonner"
+
+
+const App = () => {
+  return (
+    <div className='w-full overflow-x-hidden'>
+      {/* <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<UserLayout />}>
+            <Route index element={<Home />} />
+            <Route path='about' element={<About />} />
+            <Route path='services' element={<Services />} />
+            <Route path='contact' element={<Contact />} />
+          </Route>
+        </Routes>
+      </BrowserRouter> */}
+    </div>
+  )
+}
+
+export default App
