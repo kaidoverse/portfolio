b6f4bf8fe3932df7bdbf5736fbf883410ac26833
kaidoverse <kaidoverse@gmail.com>
2025-11-18 15:59:55 +0000

    set up basic page routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-// import { BrowserRouter, Route, Routes } from 'react-router-dom'
-// import UserLayout from './component/layout/UserLayout'
-// import About from './pages/About'
-// import Contact from './pages/Contact'
-// import Home from './pages/Home'
-// import Services from './pages/Services'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import Blog from './pages/Blog'
+import Resume from './pages/Resume'
+
 
 // import { Toaster } from "sonner"
 
@@ -12,16 +12,15 @@ import React from 'react'
 const App = () => {
   return (
     <div className='w-full overflow-x-hidden'>
-      {/* <BrowserRouter>
+      <BrowserRouter>
         <Routes>
-          <Route path='/' element={<UserLayout />}>
-            <Route index element={<Home />} />
-            <Route path='about' element={<About />} />
-            <Route path='services' element={<Services />} />
-            <Route path='contact' element={<Contact />} />
-          </Route>
+          <Route path='/' element={<Home />} />
+
+          <Route path='projects' element={<Projects />} />
+          <Route path='blog' element={<Blog />} />
+          <Route path='resume' element={<Resume />} />
         </Routes>
-      </BrowserRouter> */}
+      </BrowserRouter>
     </div>
   )
 }
